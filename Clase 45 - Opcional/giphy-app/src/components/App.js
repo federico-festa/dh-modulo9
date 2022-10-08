6b2b0a4dc169191842b7ce58587fedc1e8a5cfe2
federico_festa <federico.sfesta@gmail.com>
2022-10-08 00:26:44 -0300

    initial upload

---
+++ Clase 45 - Opcional/giphy-app/src/components/App.js
@@ -0,0 +1,14 @@
+import Navigation from './Navigation';
+import PageContent from './PageContent';
+import PageFeatures from './PageFeatures';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <PageContent />
+    </div>
+  );
+}
+
+export default App;
