ee79242449a12e0c169857e531c404132032dfda
federico_festa <federico.sfesta@gmail.com>
2022-10-09 20:03:34 -0300

    finished

--- Clase 45 - Opcional/giphy-app/src/components/Gif.js
+++ Clase 45 - Opcional/giphy-app/src/components/Gif.js
@@ -4,8 +4,7 @@ class Gif extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            gif: "",
-            title: ""
+            gif: []
         }
     };
 
@@ -16,17 +15,16 @@ class Gif extends Component {
             .catch(e => console.log(e))
     };
 
-    showGif = (data) => {
+    saveGif = (data) => {
         console.log(data);
         this.setState({
-            gif: data.data[2].images.downsized.url,
-            title: data.data[2].title
+            gif: data.data
         })
     };
 
     componentDidMount() {
         console.log('component did mount!');
-        this.apiCall('https://api.giphy.com/v1/gifs/trending?api_key=4LEB5MjWp8gOZHQ1ZHWxtMFwkyZ2SjBf', this.showGif)
+        this.apiCall('https://api.giphy.com/v1/gifs/trending?api_key=4LEB5MjWp8gOZHQ1ZHWxtMFwkyZ2SjBf', this.saveGif)
     };
 
     componentDidUpdate() {
@@ -35,18 +33,23 @@ class Gif extends Component {
 
     render() {
         return (
-            <div className="container" >
-                <div className="row text-center">
-                    <div className="col-lg-3 col-md-6 mb-4">
-                        <div className="card h-100">
-                            <img className="card-img-top" src={this.state.gif} alt="" />
-                            <div className="card-body">
-                                <h4 className="card-title">{this.state.title}</h4>
+            <body>
+                {this.state.gif.map(function (gif, i) {
+                    return <div className="container" >
+                        <div className="row text-center">
+                            <div className="col-lg-3 col-md-6 mb-4">
+                                <div className="card h-100">
+                                    <img key={i + gif} className="card-img-top" src={gif.images.downsized.url} alt="" />
+                                    <div className="card-body">
+                                        <h4 key={i + gif} className="card-title">{gif.title}</h4>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                }
+                )}
+            </body>
         );
     };
 }
