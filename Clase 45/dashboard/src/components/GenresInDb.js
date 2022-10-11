a45a761925211669991d283d1b8cb179cee505eb
federico_festa <federico.sfesta@gmail.com>
2022-10-10 22:57:14 -0300

    initial upload

--- Clase 45/dashboard/src/components/GenresInDb.js
+++ Clase 45/dashboard/src/components/GenresInDb.js
@@ -1,42 +1,48 @@
-import React from 'react';
-import Genre  from './Genre';
+import React, { Component } from 'react';
+import Genre from './Genre';
 
-let genres = [
-    {genre: 'Acción'},
-    {genre: 'Animación'},
-    {genre: 'Aventura'},
-    {genre: 'Ciencia Ficción'},
-    {genre: 'Comedia'},
-    {genre: 'Documental'},
-    {genre: 'Drama'},
-    {genre: 'Fantasia'},
-    {genre: 'Infantiles'},
-    {genre: 'Musical'}
-]
+class GenresInDb extends Component {
+    constructor() {
+        super();
+        this.state = {
+            genresList: []
+        }
+    }
 
-function GenresInDb(){
-    return (
-        <React.Fragment>
-                {/*<!-- Categories in DB -->*/}
-                <div className="col-lg-6 mb-4">						
+    componentDidMount() {
+        fetch('http://localhost:3001/api/genres')
+            .then(res => res.json())
+            .then(data => this.setState({ genresList: data.data }))
+            .catch(e => console.log(e))
+    }
+
+    mouse() {
+        const h6 = document.querySelector('h6');
+        h6.classList.add('bg-secondary');
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="col-lg-6 mb-4">
                     <div className="card shadow mb-4">
                         <div className="card-header py-3">
-                            <h6 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h6>
+                            <h6 onMouseOver={this.mouse} className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h6>
                         </div>
                         <div className="card-body">
                             <div className="row">
                                 {
-                                    genres.map((genre,index)=>{
-                                        return  <Genre  {...genre}  key={index} />
+                                    this.state.genresList.map((genre, index) => {
+                                        return <Genre  {...genre} key={index} />
                                     })
                                 }
                             </div>
                         </div>
-                    </div>
+                    </div>  
                 </div>
-           
-        </React.Fragment>
-    )
-
+            </React.Fragment>
+        )
+    }
 }
+
 export default GenresInDb;
